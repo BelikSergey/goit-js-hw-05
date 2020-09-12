22e5ccc6981a16c921fc068125cfdb7498717ed2
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-09-12 20:46:36 +0300

    добавляет решение 1  2 3 задачи

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,3 +1,22 @@
 "use strict";
 //
 // лажа с десктопом github
+const Account = function (login, email) {
+  this.login = login;
+  this.email = email;
+};
+Account.prototype.getInfo = function () {
+  return `login: ${this.login}, email: ${this.email}`;
+  // login: Poly, email: poly@mail.com
+};
+
+console.log(typeof Account.prototype.getInfo);
+// 'function'
+
+const mango = new Account("Mangozedog", "mango@dog.woof");
+console.log(mango.getInfo());
+// 'login : Mangozedog, email: mango@dog.woof'
+
+const poly = new Account("Poly", "poly@mail.com");
+console.log(poly.getInfo());
+// 'login : Poly, email: poly@mail.com'
--- JS/task-3.js
+++ JS/task-3.js
@@ -1,1 +1,58 @@
 "use strict";
+// Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    const arrey = this.items.push(item);
+    return arrey;
+  }
+  removeItem(item) {
+    const arrey = this.items;
+    for (let i = 0; i < arrey.length; i += 1) {
+      if (item === arrey[i]) {
+        arrey.splice(i, 1);
+        return this.items;
+      }
+    }
+  }
+}
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = ["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+const storage = new Storage(goods);
+console.log(storage);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem("Дроид");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem("Пролонгер");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
