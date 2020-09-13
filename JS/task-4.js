9633b32ff015ca0d615454d8db05cddb508f94f5
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-09-13 16:32:55 +0300

    добавляет решение 4 и 5 задачи

--- JS/task-4.js
+++ JS/task-4.js
@@ -1,1 +1,35 @@
 "use strict";
+// Write code under this line
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+  set value(str) {
+    this._value = str;
+  }
+  append(str) {
+    return (this._value = this._value + str);
+  }
+  prepend(str) {
+    return (this._value = str + this._value);
+  }
+  pad(str) {
+    return this.prepend(str) + this._value + this.append(str);
+  }
+}
+// console.log(typeof StringBuilder);
+// 'function'
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
